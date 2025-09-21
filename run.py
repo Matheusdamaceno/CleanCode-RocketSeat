609af6daccfac77a4d2c13aa2fa1c01f11d60f4a
Matheus <damacenomartins87@gmail.com>
2025-09-21 01:33:29 -0300

    new file:   run.py 	new file:   src/__init__.py 	new file:   src/main/__init__.py 	new file:   src/main/process_handle.py

---
+++ run.py
@@ -0,0 +1,10 @@
+#def add(x:int, y:float)->float:  #Anotaçções ou type hint, que é possivel deixar no codigo qual tipo de variavel queremos que seja passavel para um variavel x, no nosso casso int pra x e float pra y, e colocamos tambem que queremos que a função retorno um float.
+#  return x+y
+
+#val = add("ola","mundo")
+#print(val)
+
+from src.main.process_handle import start
+
+if __name__ == "__main__":
+  start()
---
+++ src/main/process_handle.py
@@ -0,0 +1,8 @@
+def start()-> None:
+  while True:
+
+      command=input()
+      if command == '1' : print("Inserindo musica")
+      elif command == '2' : print("Criando Playlist")
+      elif command == '5' : print("Saindo"), exit()
+      else: print("\n comando nao encontrado, tente novamente\n\n " )
