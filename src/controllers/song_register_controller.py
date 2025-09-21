e4ae48280dd1685827256faccfa46d75010af685
Matheus <damacenomartins87@gmail.com>
2025-09-21 14:16:50 -0300

    feat: Implementing son_register_controller

---
+++ src/controllers/song_register_controller.py
@@ -0,0 +1,42 @@
+class SongRegisterController:
+  def insert(self, new_song_informations:dict) ->dict:
+    #principio da responsabilidade unica
+    try:
+      self.__verify_songs_infos(new_song_informations)
+      self.__verify_if_song_already_registered(new_song_informations)
+      self.__insert_song(new_song_informations)
+      return self.__format_response(new_song_informations) 
+    except Exception as exception:
+      return self.__format_error_response(exception)
+
+  def __verify_songs_infos(self, new_song_informations:dict)->None:
+    if len(new_song_informations["title"]) > 100:
+      raise Exception("Titulo de musica com mais de 100 caracteres")
+    
+    year=int(new_song_informations["year"])
+    if year >= 2026:
+      raise Exception("Ano de musica invalido")
+    
+
+  def __verify_if_song_already_registered(self, new_song_informations: dict) -> None:
+    #interecao com models
+    pass
+
+  def __insert_song(self, new_song_informations: dict) -> None:
+    #interecao com models
+    pass
+
+  def __format_response(self, new_song_informations: dict) -> dict:
+    return {
+      "succes":True,
+      "count":1,
+      "attributes":{
+        "title":new_song_informations["title"]
+      }
+    }
+  
+  def __format_error_response(self, err:Exception) -> dict:
+    return{
+        "success ": False,
+        "error": str(err)
+    }
