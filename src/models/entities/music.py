0023781218fcaec573880c7825dac29445aaf1d3
Matheus <damacenomartins87@gmail.com>
2025-09-22 00:06:46 -0300

    feat: Implementing Model dir with repository and entities for musics

---
+++ src/models/entities/music.py
@@ -0,0 +1,7 @@
+class Music:
+  def __init__(self, title: str, artist: str, year: int):
+    self.title = title
+    self.artist = artist
+    self.year = year
+
+music1 = Music("title, artinst", 1999)  
