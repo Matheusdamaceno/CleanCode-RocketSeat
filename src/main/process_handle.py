47ee8ba448e60d52a5904b241c163ec612146d8f
Matheus <damacenomartins87@gmail.com>
2025-09-22 02:24:51 -0300

    Completing the project!

--- src/main/process_handle.py
+++ src/main/process_handle.py
@@ -1,11 +1,12 @@
 from src.view.first_view import introduction_page
 from .contructor.song_register_contructor import song_register_process
+from .contructor.playlist_creator_contructor import playlist_creator_process
 
 def start()-> None:
   while True:
 
       command=introduction_page()
       if command == '1' : song_register_process()
-      elif command == '2' : print("Criando Playlist")
+      elif command == '2' : playlist_creator_process()
       elif command == '5' : print("Saindo"), exit()
       else: print("\n comando nao encontrado, tente novamente\n\n " )
